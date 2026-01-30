ffc11c1d98e76ffff9cdd5aa9271a33339b30299
Victor Contreras <victorknight34@gmail.com>
2026-01-30 15:01:01 -0600

    tercer commit

--- src/lab2p2_victorcontrearas/Carro.java
+++ src/lab2p2_victorcontrearas/Carro.java
@@ -4,10 +4,103 @@
  */
 package lab2p2_victorcontrearas;
 
-/**
- *
- * @author victo
- */
+    private static String PalabraP;
+
+
 public class Carro {
+    String tipo;
+    String color;
+    double saldo;
+    boolean pagado;
+    int x, y;
+    int id;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isPagado() {
+        return pagado;
+    }
+
+    public void setPagado(boolean pagado) {
+        this.pagado = pagado;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Carro(String tipo, String color, double saldo, boolean pagado, int x, int y, int id) {
+        this.tipo = tipo;
+        this.color = color;
+        this.saldo = saldo;
+        this.pagado = pagado;
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
     
 }
+public static boolean pagar(int saldo){
+    if (saldo>0) {
+        System.out.println("El pago ha sido exitoso");
+        return true;
+    }else{
+        System.out.println(" No ha pagado todavia ");
+        return false;
+    }
+}
+public static String representacion(String tipo){
+    for (int i = 0; i <1; i++) {
+        char PalabraP = tipo.charAt(i);
+    }
+    return PalabraP;
+}
+public static void cambiarCoordenadasCarro(int fila, int columna){
+    
+}
+public static void retirarCarro(){
+    
+}
+
--- src/lab2p2_victorcontrearas/Lab2P2_VictorContrearas.java
+++ src/lab2p2_victorcontrearas/Lab2P2_VictorContrearas.java
@@ -3,18 +3,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package lab2p2_victorcontrearas;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-/**
- *
- * @author victo
- */
 public class Lab2P2_VictorContrearas {
-
-    /**
-     * @param args the command line arguments
-     */
+public static Scanner leer = new Scanner(System.in);
     public static void main(String[] args) {
+        estacionamiento();
+                
         
     }
-    
+    public static void menu(){
+        System.out.println(" Ingresar carro ");
+        System.out.println(" Marca de su carro: ");
+        String tipo = leer.next(); 
+        System.out.println(" Color de su carro: ");
+        String color = leer.next();
+        System.out.println(" Saldo a pagar: ");
+        double saldo = leer.nextDouble();
+        
+        /*System.out.println("---Opciones---");
+        System.out.println("1.Nuevo carro");
+        System.out.println("2. Sacar carro del estacionamiento");
+        System.out.println("3. Ver carros creados sin ingresar");
+        System.out.println("5. Salir");*/
+        
+    }
+    public static void estacionamiento(){
+        System.out.println("Ingrese el tamaño del estacionamiento ");
+        int n = leer.nextInt();
+        if (n>3) {
+            for (int i = 0; i < n; i++) {
+                System.out.println();
+                for (int j = 0; j < n; j++) {
+                    System.out.print(" - ");
+                }
+                
+            }
+            
+            
+        }
+        
+    }
 }
