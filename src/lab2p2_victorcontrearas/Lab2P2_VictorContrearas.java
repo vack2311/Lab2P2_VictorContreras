921099c0b33ef39f4b7fbe156f4015875e0ac9c7
Victor Contreras <victorknight34@gmail.com>
2026-01-30 13:01:13 -0600

    first commit

---
+++ src/lab2p2_victorcontrearas/Lab2P2_VictorContrearas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2p2_victorcontrearas;
+
+/**
+ *
+ * @author victo
+ */
+public class Lab2P2_VictorContrearas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
